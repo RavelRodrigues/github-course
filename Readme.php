c1a8a7b3392b624c0594b07f501a6f3ca114f517
Ravel Rodrigues <ravelrodriguesp@gmail.com>
2025-03-05 20:46:43 -0300

    Atualização final

---
+++ Readme.php
@@ -0,0 +1,44 @@
+TDE 01 - DESENVOLVIMENTO WEB BACK-END
+
+PROBLEMA A- ALTURA MINIMA
+
+dados retirados da questao:
+quantidade de brinquedos: n
+
+<?ph=0;
+$n_brinquedos;
+$altura_carlitos;
+$altura_brinquedos; 
+$pode_ir;
+
+for($i=0; $i<n; i++){
+    if(altura_carlitos >= altura_brinquedos[$i]{
+        $pode_ir++
+    }
+}
+
+echo $pode_ir
+
+
+
+
+
+
+
+
+
+PROBLEMA A- TELEFERICO
+
+$c = 100;
+$a = 87;
+$resultado_final = 0;
+
+$resultado_final = ceil($a / ($c - 1));
+
+echo $resultado_final;
+
+
+
+
+
+
